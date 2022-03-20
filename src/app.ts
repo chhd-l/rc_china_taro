b00bb447d46c264edb03417cf81e0a2288f1256c
朱宇奇 <396838319@qq.com>
2022-03-20 21:43:53 +0800

    code review 1

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import './app.less'
 const App = (props) => {
   useEffect(() => {
     console.log(props,'12345')
+    console.log(props,'12345')
+    console.log(props,'12345')
   }, [])
   return props.children
 }
