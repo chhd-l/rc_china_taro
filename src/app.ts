700277148cca19f5d5b741db135fbf9ccfec8373
zuoqin <qzuo@deloitte.com.cn>
2022-03-25 16:51:04 +0800

    session 引入

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
+import "@/utils/global.ts"
+import "windi.css";
 import "taro-ui/dist/style/index.scss";
 import "./app.less";
-import "windi.css";
 
 const App = (props) => {
   useEffect(() => {
--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,9 +1,11 @@
+import Announcement from "@/components/common/Announcement";
 import { View, Text } from "@tarojs/components";
 import "./index.less";
 
 const Account = () => {
   return (
-    <View className='index'>
+    <View className="index">
+      <Announcement title="添加社群，畅享更多专属福利！" />
       <Text>my account</Text>
     </View>
   );
