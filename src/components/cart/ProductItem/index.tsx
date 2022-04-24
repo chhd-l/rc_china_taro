0e25e33ee7594bc35d9c7bf459feb45c58dc40e4
zuoqin <qzuo@deloitte.com.cn>
2022-04-24 13:20:55 +0800

    cart 样式

--- src/components/cart/ProductItem/index.tsx
+++ src/components/cart/ProductItem/index.tsx
@@ -31,52 +31,59 @@ const ProductItem = ({
             text: '删除',
             style: {
               backgroundColor: '#FF4949',
+              width: '40px',
             },
           },
         ]}
         autoClose
         onClick={delCartProduct}
-        areaWidth={Taro.getSystemInfoSync().windowWidth + 40}
-        maxDistance={40}
+        areaWidth={Taro.getSystemInfoSync().windowWidth}
+        maxDistance={70}
       >
-        <View className="flex flex-row items-center p-2">
-          <Radio
-            value="选中"
-            checked={select}
-            style={{ transform: 'scale(0.6)' }}
-            color="red"
-            className="text-48"
-            onClick={() => changeProduct && changeProduct(id, 'select', !select)}
-          />
-          <Image
-            className="w-20 h-20"
-            src={image}
-            onClick={() => {
-              Taro.redirectTo({ url: `/pages/packageA/productDetail/index?id=${product.skuGoodInfo.id}` })
-            }}
-          />
-          <View className="ml-2">
-            <View className="font-semibold text-32 text-black">{product.skuGoodInfo.goodsName}</View>
-            <View className="mt-1 font-semibold text-black">{specs}</View>
-            <View className="flex flex-row mt-1 text-20">
-              {tags.map((el) => (
-                <View className="border border-solid rounded-md border-red-500 mr-2 px-1 text-red-500">{el}</View>
-              ))}
-            </View>
-            <View className="flex flex-row mt-1 justify-between items-center">
-              <View className="text-red-500 font-medium text-base">{formatMoney(price)}</View>
-              <View>
-                <AtInputNumber
-                  min={0}
-                  max={10}
-                  step={1}
-                  value={goodsNum}
-                  onChange={(value) => {
-                    changeProduct && changeProduct(id, 'goodsNum', value)
-                  }}
-                  type="number"
-                  className="rc-input-number"
-                />
+        <View
+          style={{
+            width: Taro.getSystemInfoSync().windowWidth + 'px',
+          }}
+        >
+          <View className="w-full flex flex-row items-center p-2">
+            <Radio
+              value="选中"
+              checked={select}
+              style={{ transform: 'scale(0.6)' }}
+              color="red"
+              className="text-48"
+              onClick={() => changeProduct && changeProduct(id, 'select', !select)}
+            />
+            <Image
+              className="w-20 h-20"
+              src={image}
+              onClick={() => {
+                Taro.redirectTo({ url: `/pages/packageA/productDetail/index?id=${product.skuGoodInfo.id}` })
+              }}
+            />
+            <View className="ml-2 w-full">
+              <View className="font-semibold text-32 text-black">{product.skuGoodInfo.goodsName}</View>
+              <View className="mt-1 font-semibold text-black">{specs}</View>
+              <View className="flex flex-row mt-1 text-20">
+                {tags.map((el) => (
+                  <View className="border border-solid rounded-md border-red-500 mr-2 px-1 text-red-500">{el}</View>
+                ))}
+              </View>
+              <View className="flex flex-row mt-1 justify-between items-center">
+                <View className="text-red-500 font-medium text-base">{formatMoney(price)}</View>
+                <View style={{ marginRight: '20px' }}>
+                  <AtInputNumber
+                    min={0}
+                    max={10}
+                    step={1}
+                    value={goodsNum}
+                    onChange={(value) => {
+                      changeProduct && changeProduct(id, 'goodsNum', value)
+                    }}
+                    type="number"
+                    className="rc-input-number"
+                  />
+                </View>
               </View>
             </View>
           </View>
