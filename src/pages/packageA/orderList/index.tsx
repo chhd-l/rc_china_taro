8da3f26ddbe8bfdde5acb85e126858c2ea5f4a95
chhd <xscnwan13@163.com>
2022-04-25 17:53:04 +0800

    订单列表和订单详情页面

---
+++ src/pages/packageA/orderList/index.tsx
@@ -0,0 +1,40 @@
+import Taro, { getCurrentInstance } from '@tarojs/taro'
+import { useState } from 'react'
+import { AtTabs, AtTabsPane } from 'taro-ui'
+import OrderListComponents from '@/components/order/OrderListComponents'
+import './index.less'
+
+const tabList = [{ title: '全部' }, { title: '待付款' }, { title: '待发货' }, { title: '待收货' }]
+const OrderList = () => {
+  const [current, setCurrent] = useState(0)
+  const { router } = getCurrentInstance()
+
+  Taro.useDidShow(() => {
+    const index = Number(router?.params?.index) || 0
+    console.log('index', index)
+    setCurrent(index)
+  })
+
+  const handleClick = (value) => {
+    setCurrent(value)
+  }
+
+  return (
+    <AtTabs className="index" current={current} tabList={tabList} onClick={handleClick} swipeable>
+      <AtTabsPane current={current} index={0}>
+        <OrderListComponents list={['']} />
+      </AtTabsPane>
+      <AtTabsPane current={current} index={1}>
+        <OrderListComponents list={['']} />
+      </AtTabsPane>
+      <AtTabsPane current={current} index={2}>
+        <OrderListComponents list={['']} />
+      </AtTabsPane>
+      <AtTabsPane current={current} index={3}>
+        <OrderListComponents list={['']} />
+      </AtTabsPane>
+    </AtTabs>
+  )
+}
+
+export default OrderList
