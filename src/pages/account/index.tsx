e6db8e12cafcef679c3ee0706663eee6a861e720
zuoqin <qzuo@deloitte.com.cn>
2022-04-26 20:14:35 +0800

    查询不同订单状态的订单列表

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -19,9 +19,9 @@ interface OrderTypeProps {
 }
 
 const orderTypeList: OrderTypeProps[] = [
-  { label: '待付款', icon: defaultIcon, url: '/pages/packageA/orderList/index?index=1' },
-  { label: '待发货', icon: defaultIcon, url: '/pages/packageA/orderList/index?index=2' },
-  { label: '待收货', icon: defaultIcon, url: '/pages/packageA/orderList/index?index=3' },
+  { label: '待付款', icon: defaultIcon, url: '/pages/packageA/orderList/index?status=UNPAID' },
+  { label: '待发货', icon: defaultIcon, url: '/pages/packageA/orderList/index?status=TO_SHIP' },
+  { label: '待收货', icon: defaultIcon, url: '/pages/packageA/orderList/index?status=SHIPPED' },
   { label: '退货/退款', icon: defaultIcon, url: '' },
   { label: '我的卡券', icon: defaultIcon, url: '' },
 ]
@@ -96,7 +96,7 @@ const Account = () => {
               className="text-xs"
               onClick={() => {
                 Taro.navigateTo({
-                  url: `/pages/packageA/orderList/index?index=0`,
+                  url: `/pages/packageA/orderList/index?status=ALL`,
                 })
               }}
             >
--- src/pages/packageA/orderList/index.tsx
+++ src/pages/packageA/orderList/index.tsx
@@ -1,5 +1,5 @@
 import Taro, { getCurrentInstance } from '@tarojs/taro'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { AtTabs, AtTabsPane } from 'taro-ui'
 import OrderListComponents from '@/components/order/OrderListComponents'
 import { getOrderList } from '@/framework/api/order/order'
@@ -8,32 +8,36 @@ import './index.less'
 
 const tabList = [{ title: '全部' }, { title: '待付款' }, { title: '待发货' }, { title: '待收货' }]
 
+const OrderStatusEnum = {
+  ALL: 0,
+  UNPAID: 1,
+  TO_SHIP: 2,
+  SHIPPED: 3,
+}
+
 const OrderList = () => {
-  const [current, setCurrent] = useState(0)
+  const [current, setCurrent] = useState('ALL')
   const [orderList, setOrderList] = useState<Order[]>([])
   const { router } = getCurrentInstance()
 
-  const getOrderLists = async () => {
+  const getOrderLists = async (status) => {
     const res = await getOrderList({
       storeId: '12345678',
       operator: 'zz',
       limit: 10,
       offset: 0,
       isNeedTotal: true,
-      sample: {},
+      sample: Object.assign({}, status !== 'ALL' ? { orderState: status } : {}),
     })
     console.log('order list data', res)
     setOrderList(res?.records)
   }
 
-  useEffect(() => {
-    getOrderLists()
-  }, [])
-
   Taro.useDidShow(() => {
-    const index = Number(router?.params?.index) || 0
-    console.log('index', index)
-    setCurrent(index)
+    const status = router?.params?.status || 'ALL'
+    console.log('status', status)
+    setCurrent(status)
+    getOrderLists(status)
   })
 
   const handleClick = (value) => {
@@ -41,9 +45,9 @@ const OrderList = () => {
   }
 
   return (
-    <AtTabs className="index" current={current} tabList={tabList} onClick={handleClick} swipeable>
+    <AtTabs className="index" current={OrderStatusEnum[current]} tabList={tabList} onClick={handleClick} swipeable>
       {tabList.map((item, index) => (
-        <AtTabsPane current={current} index={index} key={item.title}>
+        <AtTabsPane current={OrderStatusEnum[current]} index={index} key={item.title}>
           {orderList.length > 0 ? <OrderListComponents list={orderList} /> : null}
         </AtTabsPane>
       ))}
