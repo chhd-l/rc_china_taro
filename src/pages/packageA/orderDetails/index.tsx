e7d8530b6fb87d364c6bf245a92ca6b51c4183de
tqx <1585224134@qq.com>
2022-05-06 21:38:05 +0800

    upd: numcolor=>#c6c6c6

--- src/pages/packageA/orderDetails/index.tsx
+++ src/pages/packageA/orderDetails/index.tsx
@@ -125,7 +125,7 @@ const OrderDetails = () => {
                           <View className="px-1 border rounded-lg border-solid border-red-500 mr-2 mt-2">{tag}</View>
                         ))}
                       </View>
-                      <View>X{el?.num}</View>
+                      <View className="numcolor">X{el?.num}</View>
                     </View>
                     <View className="mt-2 ProductIntroduction">规格：{el?.goodsSpecifications}</View>
                   </View>
