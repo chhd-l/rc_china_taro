1dde1437bda96d1032cf8491923c1e08fbb30bb1
zuoqin <qzuo@deloitte.com.cn>
2022-05-12 09:59:21 +0800

    bug fixed

--- src/components/checkout/TotalCheck/index.tsx
+++ src/components/checkout/TotalCheck/index.tsx
@@ -21,7 +21,7 @@ const TotalCheckout = ({
   return (
     <View className=" totalcheck flex flex-row items-end justify-end border-t border-b-0 border-l-0 border-r-0 border-solid border-gray-400 w-full p-2 z-10 bg-gray-100 z-10">
       <Text className="totalprice text-gray-400 text-xs">共{num}件商品 总计：</Text>
-      <Text className="text-red-500  text-2xl font-black mr-4">{formatMoney(totalPrice)}</Text>
+      <Text className="text-primary-red  text-2xl font-black mr-4">{formatMoney(totalPrice)}</Text>
       <AtButton
         type="primary"
         className="total-settle-button w-28 h-24 rc-button total-check-button"
