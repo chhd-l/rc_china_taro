2413fc8e4fffbd9fd11efd5de2b23fb813d503af
tutu <1515112287@qq.com>
2022-05-19 14:42:51 +0800

    添加iconfont图标库

--- src/components/creatSubscription/Card/index.tsx
+++ src/components/creatSubscription/Card/index.tsx
@@ -2,13 +2,14 @@ import { petInfoAtom } from "@/store/subscription"
 import { Text, View } from "@tarojs/components"
 import { useAtom } from "jotai"
 import { useEffect, useState } from "react"
-import { AtIcon, AtTag } from "taro-ui"
+import { AtTag } from "taro-ui"
+import IconFont from '@/iconfont';
 import './index.less'
 
 const CardTypeList = [
-  { value: 0, title: '季卡', desc: '低至3.9元/天', count: 3, price: 442, day: 114, line: 519 },
-  { value: 1, title: '半年卡', desc: '低至3.9元/天', count: 5, price: 692, day: 190, line: 865 },
-  { value: 2, title: '年卡', desc: '低至3.9元/天', count: 10, price: 1298, day: 380, line: 1730 }]
+  { value: 0, title: '季卡', desc: '低至3.9元/天', count: 3, price: 442, day: 114, line: 519, iconName: 'jiduka' },
+  { value: 1, title: '半年卡', desc: '低至3.9元/天', count: 5, price: 692, day: 190, line: 865, iconName: 'bannianka' },
+  { value: 2, title: '年卡', desc: '低至3.9元/天', count: 10, price: 1298, day: 380, line: 1730, iconName: 'nianka' }]
 const Card = () => {
   const [cardType, setCardType] = useState(0)
   const [petInfo, setPetInfo] = useAtom(petInfoAtom)
@@ -27,7 +28,8 @@ const Card = () => {
             setPetInfo({ ...petInfo, discountPrice: item.price, originalPrice: item.line })
 
           }}>
-            <View className='at-icon at-icon-settings text-xl font-bold'>{item.title}</View>
+            <View className='flex flex-row text-xl font-bold items-center'>
+              <IconFont name={item.iconName} size={50} />{item.title}</View>
             <View className="lowAsDay">{item.desc}</View>
           </View>
         ))
@@ -43,7 +45,8 @@ const Card = () => {
 
           }}>
             <View className={` pt-2 pb-4 cardContent ${index == cardType && 'cardContent_checked'}`}>
-              <AtIcon value='check-circle' size='12' color='#ffff'></AtIcon>
+              <IconFont name="bannianka" />
+              33
               <View className={` items-center  flex flex-col`}>
                 <AtTag type='primary' circle className={`mx-1 cardTag ${index == cardType && 'cardTag_checked'} `}>{item.count}包订阅价</AtTag>
                 <View>
