7a046d49626121c36592c7b558217e738e88ba9a
zuoqin <qzuo@deloitte.com.cn>
2022-05-25 15:24:17 +0800

    voucher 接口联调

--- src/framework/api/voucher/voucher.ts
+++ src/framework/api/voucher/voucher.ts
@@ -2,59 +2,45 @@ import { dataSource } from '@/mock/voucher'
 import Mock from 'mockjs'
 import { Voucher } from '@/framework/types/voucher'
 import moment from 'moment'
-import ApiRoot, { baseSetting, isMock } from '../fetcher'
+import Taro from '@tarojs/taro'
+import ApiRoot, { isMock } from '../fetcher'
 
-export const getVouchers = async () => {
-  try {
-    if (isMock) {
-      return Mock.mock(dataSource)
-    } else {
-      // const res = await ApiRoot.carts().getCarts({ customerId: baseSetting.customerId, storeId: baseSetting.storeId })
-      // const cartProducts = res?.carts || []
-      // console.log('cart products data', cartProducts)
-      // return cartProducts||[]
-      return Mock.mock(dataSource)
-    }
-  } catch (err) {
-    console.log('err', err)
-    return []
+const normalizeCheckoutAndListVouchers = (voucherList) => {
+  return {
+    notUsedVouchers: voucherList.filter((el: Voucher) => !el.isUsed),
+    usedVouchers: voucherList.filter((el: Voucher) => el.isUsed),
+    expiredVouchers: voucherList.filter((el: Voucher) => el.isExpired),
   }
 }
 
-//获取voucher list page vouchers
-export const getListVouchers = async (params) => {
+//获取voucher list and checkout page vouchers
+export const getListVouchers = async () => {
   try {
-    const voucherList = Mock.mock(dataSource).filter((el: Voucher) => {
-      return (
-        (params.status === 'NOT_USED' && !el.isUsed) ||
-        (params.status === 'USED' && el.isUsed) ||
-        (params.status === 'EXPIRED' && el.isExpired)
-      )
-    })
-    return {
-      notUsedNum: params.status === 'NOT_USED' ? voucherList.length : 0,
-      usedNum: params.status === 'USED' ? voucherList.length : 0,
-      expiredNum: params.status === 'EXPIRED' ? voucherList.length : 0,
-      records: voucherList,
+    if (isMock) {
+      return normalizeCheckoutAndListVouchers(Mock.mock(dataSource))
+    } else {
+      let { customerId } = Taro.getStorageSync('wxLoginRes')?.customerAccount
+      const res = await ApiRoot.vouchers().getCustomerVouchers({ customerId })
+      let vouchers = res?.consumerVoucherDetailList || []
+      vouchers = vouchers.map((el) => normalizeVoucher(el, 'list'))
+      console.log('customer vouchers view data', vouchers)
+      return normalizeCheckoutAndListVouchers(vouchers)
     }
   } catch (err) {
     console.log('err', err)
     return {
-      notUsedNum: 0,
-      usedNum: 0,
-      expiredNum: 0,
-      records: [],
+      notUsedVouchers: [],
+      usedVouchers: [],
+      expiredVouchers: [],
     }
   }
 }
 
 //获取PDP page vouchers
-export const getPdpVouchers = async ({ goodsId }) => {
+export const getPdpVouchers = async (params) => {
   try {
-    const res = await ApiRoot.vouchers().getVouchersByGoodsId({
-      goodsId,
-      storeId: baseSetting.storeId,
-    })
+    let { customerId, storeId } = Taro.getStorageSync('wxLoginRes')?.customerAccount
+    const res = await ApiRoot.vouchers().getVouchersByGoodsId({ ...params, customerId, storeId })
     let vouchers = res?.voucherDetailList || []
     vouchers = vouchers.map((el) => normalizeVoucher(el, 'pdp'))
     console.log('PDP vouchers view data', vouchers)
@@ -78,21 +64,34 @@ const normalizeVoucher = (voucher: any, origin: string) => {
     discountType,
   } = voucher
   return {
-    id: id,
+    id: origin === 'pdp' ? id : voucher.voucherId,
     voucherPrice:
       discountType === 'PERCENTAGE' && Number(discountValue) < 1
         ? Number(discountValue) * Number(minimumBasketPrice)
-        : Number(discountValue), //优惠价格
+        : Number(discountValue || 0), //优惠价格
     voucherName: voucherName, //优惠券name
     voucherDescription: voucherDescription, //优惠券描述
     expiredTime:
       moment(voucherUsageBeginningOfTime).format('YYYY.MM.DD') +
       '-' +
       moment(voucherUsageEndOfTime).format('YYYY.MM.DD'), //领取/失效时间
-    isReceived: origin === 'pdp' ? false : origin === 'checkout' ? true : true, //是否已领取
-    isExpired: voucherStatus === 'Expired', //是否已失效
+    isReceived: origin === 'pdp' ? voucher?.isGetStatus : true, //是否已领取
+    isExpired: origin === 'pdp' ? voucherStatus === 'Expired' : voucherStatus === '2', //是否已失效
     isSelect: false, //是否在checkout页面已选择
-    voucherUsePrice: Number(minimumBasketPrice), //达到多少钱可使用优惠券
-    isUsed: origin === 'pdp' ? false : origin === 'checkout' ? false : true, //是否已使用
+    voucherUsePrice: Number(minimumBasketPrice || 0), //达到多少钱可使用优惠券
+    isUsed: origin === 'pdp' ? false : voucherStatus === '1', //是否已使用
+  }
+}
+
+//领取优惠券
+export const receiveVoucher = async (params) => {
+  try {
+    const { customerId } = Taro.getStorageSync('wxLoginRes')?.customerAccount
+    const res = await ApiRoot.vouchers().receiveVoucher({ ...params, customerId })
+    console.log('receive voucher', res?.voucherReceive)
+    return res?.voucherReceive || false
+  } catch (err) {
+    console.log('err', err)
+    return false
   }
 }
