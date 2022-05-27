546594d4bd9350aa14248d8e06d17343b16f8403
tutu <1515112287@qq.com>
2022-05-27 17:41:46 +0800

    样式修改

--- src/components/creatSubscription/PriceFooter/index.tsx
+++ src/components/creatSubscription/PriceFooter/index.tsx
@@ -5,7 +5,7 @@ import { useAtom } from "jotai"
 const PriceFooter = () => {
   const [productInfo] = useAtom(recommendProductAtom)
   return <View className=" w-full h-rc96 fixed bottom-0 left-0 bg-rc_FAFBFC flex items-center justify-end py-4">
-    <Text className="line-through text-rc_222222  text-rc22 mt-2">原价￥{productInfo.originalPrice}，套餐折后价</Text>
+    <Text className=" text-rc_222222  text-rc22 mt-2"><Text className="line-through">原价￥{productInfo.originalPrice}</Text>，套餐折后价</Text>
     <Text className="text-primary-red font-bold text-rc54 mr-4 ml-1">￥{productInfo.discountPrice}</Text>
   </View>
 }
