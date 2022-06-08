00194dde8ee005f3202ee66598feb7a6a64d2965
zuoqin <qzuo@deloitte.com.cn>
2022-06-08 14:23:10 +0800

    checkout style

--- src/components/checkout/DeliveryTime/index.tsx
+++ src/components/checkout/DeliveryTime/index.tsx
@@ -16,13 +16,13 @@ const DeliveryTime = ({ changeDeliveryDate }: { changeDeliveryDate: Function }) 
     <View className="bg-white mt-2 pl-2 py-2 rounded">
       <Picker mode="date" start={deliveryTime} value={deliveryTime} onChange={onDateChange}>
         <View className="flex flex-row justify-between items-center">
-          <View className="text-30 flex flex-row items-center">
-            <Image className="w-6 h-6 mr-2" src={TO_SHIP_ORDER_ICON} />
+          <View className="text-26 flex flex-row items-center">
+            <Image className="w-5 h-5 mr-2" src={TO_SHIP_ORDER_ICON} />
             发货时间
           </View>
           <View>
             <View>
-              <Text className="text-xs text-gray-400">{deliveryTime}</Text>
+              <Text className="text-26 text-gray-400">{deliveryTime}</Text>
               <AtIcon value="chevron-right" size="24" color="#666666" />
             </View>
           </View>
