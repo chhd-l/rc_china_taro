cad113e68506b286f6d6402f70025b8ebb60e1fb
zuoqin <qzuo@deloitte.com.cn>
2022-06-09 11:09:28 +0800

    empty cart style

--- src/components/cart/Empty/index.tsx
+++ src/components/cart/Empty/index.tsx
@@ -2,8 +2,7 @@ import { Image, View } from '@tarojs/components'
 import Taro from '@tarojs/taro'
 import { AtButton } from 'taro-ui'
 import '@/pages/cart/index.less'
-import {CART_NO_DATA_ICON} from '@/lib/constants'
-import './index.less'
+import { CART_NO_DATA_ICON } from '@/lib/constants'
 
 const Empty = () => {
   return (
@@ -11,15 +10,14 @@ const Empty = () => {
       <View className="flex flex-col items-center">
         <Image className="w-40 h-28" src={CART_NO_DATA_ICON} />
         <View className="mt-2 text-base font-bold">购物车有点空</View>
-        <View className="my-2 text-xs text-gray-400">赶紧犒劳一下爱宠吧</View>
+        <View className="mb-4 text-xs text-gray-400">赶紧犒劳一下爱宠吧</View>
         <AtButton
-          className="my-button w-32"
+          className="w-32 border-primary-red bg-white text-30 text-primary-red flex items-center h-10 rounded-3xl border-2 font-semibold"
           onClick={() => {
             Taro.switchTab({
               url: '/pages/productList/index',
             })
           }}
-          customStyle={{ height: '32px',lineHeight:'30px' }}
         >
           去逛逛
         </AtButton>
