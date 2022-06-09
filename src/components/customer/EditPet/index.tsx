4eef04bc62d4c7d328eff283bbfe0d4fbd05c5a8
chhd <xscnwan13@163.com>
2022-06-09 15:08:24 +0800

    我的宠物页面bug修改

--- src/components/customer/EditPet/index.tsx
+++ src/components/customer/EditPet/index.tsx
@@ -187,14 +187,14 @@ const EditPet = ({ pet, petItem, getList, SetshowAddPetBtn, setIsEdit, petList, 
             className="col-span-8 border-solid border-gray-300 rounded-lg px-2 flex items-center"
           >
             <Picker
-              style={{ borderWidth: '0px !important' }}
+              style={{ borderWidth: '0px !important', backgroundColor: '#fff !important' }}
               mode="date"
-              className="flex-1 PickerItem w-full h-full flex items-center"
+              className="flex-1 PickerItem w-full h-full flex items-center bg-white"
               end={new Date().toLocaleString().split(' ')[0].replace(/\//g, '-')}
               onChange={handleChangeDate}
             >
-              <AtList className="border-0 w-full h-full">
-                <AtListItem className="text-xl" title="请选择生日" extraText={petInfo.birthday} />
+              <AtList className="border-0 w-full h-full bg-white">
+                <AtListItem className="text-xl bg-white" title="请选择生日" extraText={petInfo.birthday} />
               </AtList>
             </Picker>
           </View>
