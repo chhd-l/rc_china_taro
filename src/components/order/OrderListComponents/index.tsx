d038c0d0a9380c6375e0e85c7fc6eda0f764ef7d
zuoqin <qzuo@deloitte.com.cn>
2022-06-10 11:07:48 +0800

    bug fixed

--- src/components/order/OrderListComponents/index.tsx
+++ src/components/order/OrderListComponents/index.tsx
@@ -99,7 +99,7 @@ const OrderListComponents = ({ list, openModalTip }: { list: Order[]; openModalT
                 <View className="text-primary-red flex text-20 justify-between items-center">
                   <View className="flex flex-row flex-wrap">
                     {normalizeTags(el.goodsAttributeAndValues, el.feedingDays).map((tag) => (
-                      <View className="px-1 border rounded-lg border-solid border-red mr-2 mb-1">{tag}</View>
+                      <View className="p-1 border rounded-lg border-solid border-red mr-2 mb-1">{tag}</View>
                     ))}
                   </View>
                   <View className="text-gray-400">X{el?.num}</View>
