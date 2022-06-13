735ac5b4523a68df76b5f776176823e0b84f1659
lizongji <572510582@qq.com>
2022-06-13 18:40:33 +0800

    分享海报update

--- src/components/product/ImgPoster/index.tsx
+++ src/components/product/ImgPoster/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import { View, Button, Image } from '@tarojs/components'
+import { View, Button, Image, Canvas } from '@tarojs/components'
 import Taro from '@tarojs/taro'
 import minLogo from '@/assets/img/min-logo.png'
 import { AtFloatLayout } from 'taro-ui'
 import './index.less'
 import { PDP_POSTER, PDP_WECHAT } from '@/lib/constants'
+import { formatMoney } from '@/utils/utils'
 const ImgPoster = ({ productInfo, qrcode, setShowPoster, showPoster, setShowShareBtn, showShareBtn }) => {
-  const [bgdSrc, setBgdSrc] = useState('')
   const [picTempUrl, setPicTempUrl] = useState('')
   const [posterStatus, setPosterStatus] = useState(false)
   let deviceWidth = Taro.getSystemInfoSync().screenWidth
@@ -17,7 +17,6 @@ const ImgPoster = ({ productInfo, qrcode, setShowPoster, showPoster, setShowShar
     console.info('imgimgimgimg', img)
     let res: any = await handleNetImg(img)
     let bgdSrc = res.path
-    setBgdSrc(bgdSrc) //保存这个临时图片路径
     return bgdSrc
   }
   //生成临时图片
@@ -40,42 +39,59 @@ const ImgPoster = ({ productInfo, qrcode, setShowPoster, showPoster, setShowShar
     Taro.showLoading({
       title: '正在生成中...',
     })
-    let productImg = await initPic(productInfo.img)
-    let qrcodeImg = await initPic(qrcode)
     var ctx = Taro.createCanvasContext('mycanvas', this)
     const user = Taro.getStorageSync('wxLoginRes').userInfo
-
+    let productImg = await initPic(productInfo.img)
+    let qrcodeImg = await initPic(qrcode)
+    let userAvatar = await initPic(user.avatarUrl)
     console.info('deviceWidth', deviceWidth)
     console.info('deviceHeight', deviceHeight)
+    ctx.drawImage(productImg, 0, 120, deviceWidth, deviceWidth) //产品图
+    // ctx.drawImage(productInfo.img, 0, 0, deviceWidth, deviceWidth, 30, 45, 140, 140) //画商品图片
+    ctx.drawImage(qrcodeImg, deviceWidth - 130, deviceHeight - 120, 100, 100) //画二维码
+    ctx.drawImage(minLogo, 20, deviceHeight - 120, 100, 100) //
+    //名字
+    ctx.setFillStyle('#666')
+    ctx.setFontSize(20)
+    let productName = productInfo?.name?.slice(0, 8)
+    if (productInfo?.name?.length > 8) {
+      productName = productName + '...'
+    }
+    // 原价
+    ctx.fillText(productName, 20, deviceWidth + 160)
+    ctx.setFillStyle('#999')
+    ctx.setFontSize(16)
+    let originalPrice = `原价${formatMoney(productInfo?.originalPrice)}`
+    let textWidth = ctx.measureText(originalPrice).width
+    ctx.fillText(originalPrice, deviceWidth / 2, deviceWidth + 160)
+    ctx.rect(deviceWidth / 2, deviceWidth + 151, textWidth, 1)
+    ctx.fill()
+
+    // 现价
+    let price = `${formatMoney(productInfo?.price)}`
+    ctx.setFillStyle('#d33024')
+    ctx.setFontSize(26)
+    ctx.fillText(price, deviceWidth / 2 + textWidth, deviceWidth + 160)
+
+    // 描述
+    ctx.setFillStyle('#666')
+    ctx.setFontSize(13)
+    ctx.fillText('1.保存图片到相册', 130, deviceHeight - 75)
+    ctx.fillText('2.长按到爱宠有卡查看商品', 130, deviceHeight - 55)
+
+    // 描述
+    ctx.setFillStyle('#666')
+    ctx.setFontSize(20)
+    ctx.fillText(user?.nickName || '', 130, 50)
+    ctx.fillText('为你的爱宠挑个好物，请查收', 130, 80)
 
     ctx.beginPath() //标志开始一个路径
     ctx.arc(60, 60, 40, 0, 2 * Math.PI) //在canvas中绘制圆形
     ctx.stroke()
-
-    ctx.drawImage(productImg, 0, 120, deviceWidth, deviceWidth) //画背景图
-    // ctx.drawImage(productInfo.img, 0, 0, deviceWidth, deviceWidth, 30, 45, 140, 140) //画商品图片
-    ctx.drawImage(qrcodeImg, deviceWidth - 130, deviceHeight - 120, 100, 100) //画二维码,这边可以变成小程序码
-    ctx.drawImage(minLogo, 20, deviceHeight - 120, 100, 100) //画二维码,这边可以变成小程序码
-    //名字
-    ctx.setFillStyle('#E80013')
-    ctx.setFontSize(20)
-    ctx.fillText(productInfo?.name, 20, deviceWidth + 160)
-    // 价格
-    ctx.setFillStyle('#d33024')
-    ctx.setFontSize(20)
-    ctx.fillText(productInfo?.price, deviceWidth / 2, deviceWidth + 160)
-
-    // 描述
-    ctx.setFillStyle('#d33024')
-    ctx.setFontSize(13)
-    ctx.fillText('1.保存图片到相册', 130, deviceHeight - 80)
-    ctx.fillText('2.长按到爱宠有卡查看商品', 130, deviceHeight - 50)
-
-    // 描述
-    ctx.setFillStyle('#d33024')
-    ctx.setFontSize(20)
-    ctx.fillText(user.nickName, 130, 50)
-    ctx.fillText('为你的爱宠挑个好物，请查收', 130, 80)
+    if (userAvatar) {
+      ctx.clip()
+      ctx.drawImage(userAvatar, 20, 20, 80, 80) //用户头像
+    }
     console.info('ctxctxctx', ctx)
     debugger
     //开始画画完后生成新的临时图片地址
@@ -123,11 +139,16 @@ const ImgPoster = ({ productInfo, qrcode, setShowPoster, showPoster, setShowShar
           mask: true,
         })
         setShowPoster(false)
+        setShowShareBtn(false)
       },
-      complete: () => {
-        setPosterStatus(false)
-        // this.posterStatus = false
+      fail: () => {
+        Taro.showToast({
+          title: '图片保失败',
+          icon: 'error',
+          mask: true,
+        })
       },
+      complete: () => {},
     })
   }
 
@@ -175,12 +196,12 @@ const ImgPoster = ({ productInfo, qrcode, setShowPoster, showPoster, setShowShar
           getPhotosAlbumAuth()
       }}>保存本地</View> */}
       <View class="canvas-box absolute invisible" style={{ left: '-10000', top: '-10000' }}>
-        <canvas
+        <Canvas
           canvas-id="mycanvas"
           style={{ width: deviceWidth, height: deviceHeight }}
           width={deviceWidth}
           height={deviceHeight}
-        ></canvas>
+        ></Canvas>
       </View>
       <AtFloatLayout
         isOpened={showPoster}
@@ -195,11 +216,11 @@ const ImgPoster = ({ productInfo, qrcode, setShowPoster, showPoster, setShowShar
             setPosterStatus(false)
           }}
         >
-          <View style={{ width: '60%', borderColor: '#fafafa' }} className="m-auto border-1 border-solid">
+          <View style={{ width: '60%', borderColor: '#f0f0f0' }} className="m-auto border-1 border-solid">
             <Image src={picTempUrl} mode="widthFix" className="w-full"></Image>
           </View>
           <Button
-            className="bg-primary-red text-white py-2 m-auto mt-2"
+            className="bg-primary-red text-white text-28 m-auto mt-2"
             style={{ borderRadius: '50px', width: '100px' }}
             onClick={() => {
               getPhotosAlbumAuth()
