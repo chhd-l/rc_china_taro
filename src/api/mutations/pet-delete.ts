05d00ffce1fb0247c8699dfbe1840110acccc741
lizongji <572510582@qq.com>
2022-06-16 10:39:21 +0800

    删除api文件

--- src/api/mutations/pet-delete.ts
+++
@@ -1,5 +0,0 @@
-export const petDeleteMutation = `
-  mutation deletePet ($id: String!, $operator: String!){
-    deletePetById(id: $id, operator: $operator)
-  }
-`;
--- src/api/queries/get-all-customers-query.ts
+++
@@ -1,7 +0,0 @@
-export const getAllCustomersQuery = `
-  query getAllCustomers($offset: Int = 50){
-    customers(offset: $offset) {
-      name
-    }
-  }
-`
