18e3a0f5e8d18d0c2ff776c111fec340d6b87fb6
zuoqin <qzuo@deloitte.com.cn>
2022-06-20 18:14:17 +0800

    update

---
+++ src/components/consumer/Attention/index.tsx
@@ -0,0 +1,46 @@
+import { Image, OfficialAccount, OfficialAccountProps, View } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
+import { WECHAT } from '@/lib/constants'
+import { useAtom } from 'jotai'
+import { consumerAtom } from '@/store/consumer'
+import { useState } from 'react'
+
+const Attention = () => {
+  const [consumerInfo, setConsumerInfo] = useAtom(consumerAtom)
+  const [errMsg, setErrMsg] = useState('')
+
+  return (
+    <View>
+      <View className="flex py-2 text-24 items-center bg-gray-fb">
+        <View className="w-12 h-12">
+          <Image className="w-full h-full" src={WECHAT} />
+        </View>
+        <View className="flex flex-col ml-2 items-center justify-center">
+          <View>更多有趣易懂养宠知识都在这里</View>
+          <View className="mt-1">订阅我，随时掌握宠粮发货进度</View>
+          <View>{errMsg}</View>
+        </View>
+        <View className="flex-1 flex justify-end">
+          <AtButton className="m-0 py-0" full={false} size="small" type="primary">
+            <OfficialAccount
+              onLoad={(e) => {
+                console.log('vvvvv', e)
+              }}
+              onError={(e) => {
+                console.log('dddddd', e.detail)
+                setErrMsg(JSON.stringify(e.detail))
+              }}
+              onClick={(event) => {
+                console.log('ccccc', event)
+              }}
+            >
+              去关注
+            </OfficialAccount>
+          </AtButton>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default Attention
