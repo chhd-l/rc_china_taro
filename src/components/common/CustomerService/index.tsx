f3990a340694f733e31ed1b41dbbad56da2a4cbb
zuoqin <qzuo@deloitte.com.cn>
2022-06-23 15:53:18 +0800

    customer service

--- src/components/common/CustomerService/index.tsx
+++ src/components/common/CustomerService/index.tsx
@@ -1,41 +1,49 @@
 import { Button, MovableArea, MovableView } from '@tarojs/components'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { CUSTOMER_SERVICE } from '@/lib/constants'
 import Taro from '@tarojs/taro'
 import './index.less'
 
 const CustomerService = () => {
-  const screenWidth = Taro.getSystemInfoSync().screenWidth
-  const screenHeight = Taro.getSystemInfoSync().screenHeight
+  const screenWidth = Taro.getSystemInfoSync().windowWidth
+  const screenHeight = Taro.getSystemInfoSync().windowHeight
+  const tabBarHeight = Taro.getSystemInfoSync()?.statusBarHeight || 47
+  const navBarHeight = Taro.getSystemInfoSync()?.safeArea?.top || 42
   const [xOffset, setXOffset] = useState(screenWidth)
-  const [yOffset, setYOffset] = useState(screenHeight - 300)
+  const [yOffset, setYOffset] = useState(screenHeight - 200)
+
+  useEffect(() => {
+    console.log('111111', Taro.getSystemInfoSync())
+  }, [])
 
   return (
     <MovableArea
-      className="fixed w-full z-50 pointer-events-none"
-      style={{ width: screenWidth + 'px', height: screenHeight - 180 + 'px', marginTop: '2.625rem' }}
+      className="fixed w-full z-999 pointer-events-none"
+      style={{
+        width: screenWidth + 'px',
+        height: screenHeight - tabBarHeight - navBarHeight + 'px',
+        marginTop: navBarHeight + 'px',
+      }}
     >
       <MovableView
-        className="pointer-events-auto z-50 customer-service-size"
+        className="pointer-events-auto z-999 customer-service-size bg-white"
         direction="all"
         x={xOffset}
         y={yOffset}
+        damping={5}
         outOfBounds
+        animation
         onTouchEnd={(e) => {
           console.log('mmmmmmmmmmm', e)
           const x = e.changedTouches[0].pageX
           const y = e.changedTouches[0].pageY
-          if (x < screenWidth / 2) {
-            setXOffset(0)
-          } else {
-            setXOffset(screenWidth)
-          }
-          setYOffset(y - 100)
+          setXOffset(x < screenWidth / 2 ? 0 : screenWidth)
+          setYOffset(y - 95)
         }}
       >
         <Button
           openType="contact"
-          className="customer-service-button customer-service-size bg-cover bg-no-repeat"
+          className="customer-service-button customer-service-size bg-cover bg-no-repeat z-999"
           style={{
             backgroundImage: `url(${CUSTOMER_SERVICE})`,
           }}
