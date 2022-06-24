97e6f48556156551d40f3370b5246ed83accba17
zuoqin <qzuo@deloitte.com.cn>
2022-06-24 16:56:11 +0800

    checkout page:1、order calculate price 从接口获取 2、subscription create and pay 提取 3、checkout page 优化

--- src/components/checkout/OrderPrice/index.tsx
+++ src/components/checkout/OrderPrice/index.tsx
@@ -2,37 +2,25 @@ import { View } from '@tarojs/components'
 import { formatMoney } from '@/utils/utils'
 import './index.less'
 
-const Remark = ({
-  totalPrice,
-  shipPrice,
-  discountPrice,
-  subDiscountPrice,
-  communityDiscountPrice,
-}: {
-  totalPrice: number
-  shipPrice: number
-  discountPrice: number
-  subDiscountPrice: number
-  communityDiscountPrice: number
-}) => {
+const Remark = ({ orderPrice, isCommunityVip }: { orderPrice: any; isCommunityVip: boolean }) => {
   return (
     <View className="p-4 text-24 text-gray-400 bggray mt-2 rounded pb-16">
       <View className="flex flex-row justify-between items-center mb-1">
         <View>商品总价</View>
-        <View>{formatMoney(totalPrice)}</View>
+        <View>{formatMoney(orderPrice?.productPrice || 0)}</View>
       </View>
       <View className="flex flex-row justify-between items-center mb-1">
         <View>运费</View>
-        <View>{formatMoney(shipPrice)}</View>
+        <View>{formatMoney(orderPrice?.deliveryPrice || 0)}</View>
       </View>
       <View className="flex flex-row justify-between items-center mb-1">
         <View>店铺优惠</View>
-        <View>{formatMoney(discountPrice + subDiscountPrice)}</View>
+        <View>{formatMoney(orderPrice?.discountsPrice || 0)}</View>
       </View>
-      {communityDiscountPrice ? (
+      {isCommunityVip ? (
         <View className="flex flex-row justify-between items-center">
           <View>社群VIP专享95折</View>
-          <View>{formatMoney(communityDiscountPrice)}</View>
+          <View>{formatMoney(orderPrice?.vipDiscountsPrice || 0)}</View>
         </View>
       ) : null}
     </View>
--- src/components/checkout/TotalCheck/index.tsx
+++ src/components/checkout/TotalCheck/index.tsx
@@ -1,32 +1,38 @@
 import { View, Text } from '@tarojs/components'
 import { formatMoney } from '@/utils/utils'
 import { AtButton } from 'taro-ui'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './index.less'
 
 const TotalCheckout = ({
-  num,
+  orderItems,
   totalPrice,
   checkNow,
-  loading,
 }: {
-  num: number
+  orderItems: any
   totalPrice: number
   checkNow: Function
-  loading: boolean
 }) => {
+  const [totalNum, setTotalNum] = useState(0)
+
+  const getTotalNum = () => {
+    const total = orderItems.reduce((prev, cur) => {
+      return prev + cur.productNum
+    }, 0)
+    setTotalNum(total)
+  }
+
   useEffect(() => {
-    console.log(loading)
-  }, [])
+    getTotalNum()
+  }, [orderItems])
 
   return (
     <View className=" flex flex-row items-end justify-end border-t border-b-0 border-l-0 border-r-0 border-solid border-gray-400 w-full p-2 z-10 bg-gray-100">
-      <Text className="text-gray-400 text-xs">共{num}件商品 总计：</Text>
+      <Text className="text-gray-400 text-xs">共{totalNum}件商品 总计：</Text>
       <Text className="text-primary-red text-3xl font-black">{formatMoney(totalPrice)}</Text>
       <AtButton
         type="primary"
         className="rc-button total-check-button"
-        // loading={loading}
         onClick={() => {
           checkNow && checkNow()
         }}
