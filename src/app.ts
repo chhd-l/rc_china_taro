bc26b5b43a751b33238c0244cff05e73b5bc1152
lizongji <572510582@qq.com>
2022-06-27 22:50:52 +0800

    注释clearStorageSync

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,14 @@ import '@/assets/css/custom-theme.scss'
 import './app.less'
 import './assets/css/global.less'
 import 'windi.css'
-import Taro from "@tarojs/taro";
+import Taro from "@tarojs/taro"
 
 const App = (props) => {
   useEffect(() => {
     console.log(props, '12345')
     Taro.onAppShow(() => {
       console.log('aaaaaaaaa', 'onAppShow')
-      Taro.clearStorageSync()
+      // Taro.clearStorageSync()
     })
   }, [])
 
